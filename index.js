c607173ea3d5068e4349307d3075e97a1af8bd96
Colin Richardson <richardsonjcolin@gmail.com>
2020-08-19 11:12:02 -0700

    run schedule every 30 min for API reqs

--- index.js
+++ index.js
@@ -1,48 +1,59 @@
 require('dotenv').config()
-const https = require('https')
 const schedule = require('node-schedule');
 const requestJobs = require('./lib/request-jobs')
 
-console.log('SERVICE STARTED...')
+const API = 'coinGecko'
+const jobsToRun = requestJobs[API]['jobs']
+
+// runs schedule every 10 min
+schedule.scheduleJob('*/30 * * * *', (time) => {
+  console.log(`SERVICE STARTED... ${time}`)
+  for(let reqJob in jobsToRun) {
+    const jobFn = jobsToRun[reqJob]
+    jobFn()
+  }
+})
 
 // runs schedule every 5 min
-schedule.scheduleJob('*/5 * * * *', (time) => {
-  console.log(`SCHEDULE RUNNING AT: ${time}`)
+// schedule.scheduleJob('*/5 * * * *', (time) => {
+//   console.log(`SCHEDULE RUNNING AT: ${time}`)
 
-  for(let reqJob in requestJobs) {
-    const options = requestJobs[reqJob]['options']
-    const jobFn = requestJobs[reqJob]['requestFn']
+  // for(let reqJob in jobsToRun) {
+  //   const jobFn = jobsToRun[reqJob]['jobFn']
+  //   const options = {...requestJobs[API]['options'], path: jobsToRun[reqJob]['path']}
+  //   // const json = JSON.parse(fs.readFileSync(`./${reqJob}.json`, 'utf8'))
 
-    let request = https.request(options, async (res) => {
-      const data = []
-      console.log(`SENT REQUEST TO: ${options.hostname}${options.path}`)
-      console.log(`RESPONSE STATUS: ${res.statusCode}`)
-      console.log('HEADERS: ', res.headers)
+  //   let request = https.request(options, async (res) => {
+  //     const data = []
+  //     console.log(`SENT REQUEST TO: ${options.hostname}${options.path}`)
+  //     console.log(`RESPONSE STATUS: ${res.statusCode}`)
+  //     console.log('HEADERS: ', res.headers)
 
-      res.on('data', (d) => {
-        data.push(d.toString())
-      })
+  //     res.on('data', (d) => {
+  //       data.push(d.toString())
+  //     })
 
-      res.on('end', async (err) => {
-        if (res.statusCode === 200) {
-          try {
-            // parse API json data
-            const result = JSON.parse(data.join('').trim())
-            // call custom job fn
-            jobFn(result)
-          } catch (e) {
-            console.log(`Error parsing JSON! ${e}`)
-          }
-        } else {
-          console.log('Error: ', err, `Status: ${res.statusCode}`)
-        }
-      })
-    })
+  //     res.on('end', async (err) => {
+  //       if (res.statusCode === 200) {
+  //         try {
+  //           // parse API json data
+  //           const result = JSON.parse(data.join('').trim())
+  //           // call custom job fn
+  //           jobFn(result)
+  //           // jobFn(json)
+  //         } catch (e) {
+  //           console.log(`Error parsing JSON! ${e}`)
+  //         }
+  //       } else {
+  //         console.log('Error: ', err, `Status: ${res.statusCode}`)
+  //       }
+  //     })
+  //   })
 
-    request.on('error', (err) => {
-      console.error(err);
-    })
+  //   request.on('error', (err) => {
+  //     console.error(err);
+  //   })
 
-    request.end()
-  }
-});
+  //   request.end()
+  // }
+// })
