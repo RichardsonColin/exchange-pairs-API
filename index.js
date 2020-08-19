091a898c6fb8743777a5a70a036e0896656c4730
Colin Richardson <richardsonjcolin@gmail.com>
2020-08-19 14:34:13 -0700

    add express

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
-const http = require('http')
+var express = require('express')
+var app = express()
 const schedule = require('node-schedule')
 const requestJobs = require('./lib/request-jobs')
 
@@ -17,15 +18,25 @@ schedule.scheduleJob('*/29 * * * *', (time) => {
   }
 })
 
-// Run server
-const PORT = process.env.PORT || 3000
-const HOST = 'localhost'
-const requestListener = (req, res) => {
-  res.writeHead(200)
-  res.end("")
-}
-const server = http.createServer(requestListener)
-server.listen(PORT, HOST, () => {
-  console.log(`Server is running on http://${HOST}:${PORT}`)
+// SERVER
+
+// Root route
+app.get('/', function (req, res) {
+  res.status(200).send('Become one with the universe.')
 })
+
+// Run server
+app.listen(PORT, () => {
+  console.log(`Server running on http://${HOST}:${PORT}`)
+})
+// const PORT = process.env.PORT || 3000
+// const HOST = 'localhost'
+// const requestListener = (req, res) => {
+//   res.writeHead(200)
+//   res.end("")
+// }
+// const server = http.createServer(requestListener)
+// server.listen(PORT, HOST, () => {
+//   console.log(`Server is running on http://${HOST}:${PORT}`)
+// })
 
