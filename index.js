a79759737463e3a6962d982e331a9c8766a91247
Colin Richardson <richardsonjcolin@gmail.com>
2020-08-19 14:00:51 -0700

    add server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
-const schedule = require('node-schedule');
+const http = require('http')
+const schedule = require('node-schedule')
 const requestJobs = require('./lib/request-jobs')
 
 const API = 'coinGecko'
@@ -7,7 +8,7 @@ const jobsToRun = requestJobs[API]['jobs']
 
 console.log('SERVICE STARTED...')
 
-// runs schedule every 10 min
+// Runs schedule every 10 min
 schedule.scheduleJob('*/10 * * * *', (time) => {
   console.log(`RUNNING... ${time}`)
   for(let reqJob in jobsToRun) {
@@ -16,46 +17,16 @@ schedule.scheduleJob('*/10 * * * *', (time) => {
   }
 })
 
-// runs schedule every 5 min
-// schedule.scheduleJob('*/5 * * * *', (time) => {
-//   console.log(`SCHEDULE RUNNING AT: ${time}`)
+// Run server
+const PORT = process.env.SERVER_PORT || 3000
+const HOST = 'localhost'
+const requestListener = (req, res) => {
+  res.writeHead(200)
+  res.end("")
+}
 
-  // for(let reqJob in jobsToRun) {
-  //   const jobFn = jobsToRun[reqJob]['jobFn']
-  //   const options = {...requestJobs[API]['options'], path: jobsToRun[reqJob]['path']}
-  //   // const json = JSON.parse(fs.readFileSync(`./${reqJob}.json`, 'utf8'))
+const server = http.createServer(requestListener)
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${HOST}:${PORT}`)
+})
 
-  //   let request = https.request(options, async (res) => {
-  //     const data = []
-  //     console.log(`SENT REQUEST TO: ${options.hostname}${options.path}`)
-  //     console.log(`RESPONSE STATUS: ${res.statusCode}`)
-  //     console.log('HEADERS: ', res.headers)
-
-  //     res.on('data', (d) => {
-  //       data.push(d.toString())
-  //     })
-
-  //     res.on('end', async (err) => {
-  //       if (res.statusCode === 200) {
-  //         try {
-  //           // parse API json data
-  //           const result = JSON.parse(data.join('').trim())
-  //           // call custom job fn
-  //           jobFn(result)
-  //           // jobFn(json)
-  //         } catch (e) {
-  //           console.log(`Error parsing JSON! ${e}`)
-  //         }
-  //       } else {
-  //         console.log('Error: ', err, `Status: ${res.statusCode}`)
-  //       }
-  //     })
-  //   })
-
-  //   request.on('error', (err) => {
-  //     console.error(err);
-  //   })
-
-  //   request.end()
-  // }
-// })
