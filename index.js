da39e5883dabdbf45763c86f635846ad98d3e353
Colin Richardson <richardsonjcolin@gmail.com>
2020-08-19 21:12:10 -0700

    run every hour

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const jobsToRun = requestJobs[API]['jobs']
 
 // Runs schedule every 1 hour
 console.log('SERVICE STARTED...')
-schedule.scheduleJob('* * */1 * * *', (time) => {
-  console.log(`RUNNING... ${time}`)
-  for(let reqJob in jobsToRun) {
-    const jobFn = jobsToRun[reqJob]
-    jobFn()
-  }
-})
+// schedule.scheduleJob('* * */1 * * *', (time) => {
+//   console.log(`RUNNING... ${time}`)
+//   for(let reqJob in jobsToRun) {
+//     const jobFn = jobsToRun[reqJob]
+//     jobFn()
+//   }
+// })
 
 // ---------------------------------------------------------------------
 
