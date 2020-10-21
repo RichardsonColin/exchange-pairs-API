496a094ce83100636ddca5f4fcb5a81940212ee8
Colin Richardson <richardsonjcolin@gmail.com>
2020-10-21 12:46:30 -0700

    modify insert query to an upsert

--- models/exchange-asset-pair-reference.js
+++ models/exchange-asset-pair-reference.js
@@ -3,20 +3,33 @@ const db = require('../db')
 module.exports = {
   createMany: async data => {
     try {
-      const queryStatement = `INSERT INTO exchange_asset_pair_references (exchange_id, asset_symbol, quote_symbol, is_stale)
+      const queryStatement = `INSERT INTO exchange_asset_pair_references (exchange_id, asset_symbol, quote_symbol, asset_id, quote_id, is_stale)
         SELECT
           (SELECT id FROM exchanges WHERE api_id = $1),
           $2,
           $3,
-          $4
+          (SELECT id FROM assets WHERE symbol = $4),
+          (SELECT id FROM assets WHERE symbol = $5),
+          $6
         WHERE (SELECT id FROM exchanges WHERE api_id = $1) IS NOT NULL
-        ON CONFLICT DO NOTHING;`
+        AND (SELECT id FROM assets WHERE symbol = $4) IS NOT NULL
+        AND (SELECT id FROM assets WHERE symbol = $5) IS NOT NULL
+        ON CONFLICT(exchange_id, asset_symbol, quote_symbol)
+        DO UPDATE
+        SET exchange_id = (SELECT id FROM exchanges WHERE api_id = $1),
+          asset_symbol = $2,
+          quote_symbol = $3,
+          asset_id = (SELECT id FROM assets WHERE symbol = $4),
+          quote_id = (SELECT id FROM assets WHERE symbol = $5),
+          is_stale = $6;`
       const finalizedQueries = []
 
       for(let pairRef of data) {
         finalizedQueries.push(db.query(queryStatement,
           [
             pairRef.exchangeId,
+            pairRef.assetSymbol,
+            pairRef.quoteSymbol,
             pairRef.assetSymbol,
             pairRef.quoteSymbol,
             pairRef.isStale
