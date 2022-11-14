4813cdc9aebdc9944347ea9edc5c40cdaa2641ab
Colin Richardson <colin.richardson.dev@gmail.com>
2022-11-14 16:27:29 -0600

    move query strings outside try/catch for log

--- models/asset.js
+++ models/asset.js
@@ -1,29 +1,32 @@
-const db = require('../db')
+const db = require('../db');
 
 module.exports = {
   getAll: async () => {
+    const queryStatement = 'SELECT * FROM assets;';
     try {
-      const queryStatement = "SELECT * FROM assets;";
-      const { rows } = await db.query(queryStatement, [])
-      return rows
+      const { rows } = await db.query(queryStatement, []);
+      return rows;
     } catch (err) {
       // log err
-      console.log(`${queryStatement}: `, err)
+      console.log(`${queryStatement}: `, err);
     }
   },
-  createMany: async data => {
+  createMany: async (data) => {
+    const queryStatement =
+      'INSERT INTO assets (api_id, symbol, name) VALUES (LOWER($1), $2, $3) ON CONFLICT DO NOTHING;';
     try {
-      const queryStatement = "INSERT INTO assets (api_id, symbol, name) VALUES (LOWER($1), $2, $3) ON CONFLICT DO NOTHING;";
-      const finalizedQueries = []
+      const finalizedQueries = [];
 
-      for(let asset of data) {
-        finalizedQueries.push(db.query(queryStatement, [asset.id, asset.symbol, asset.name]))
+      for (let asset of data) {
+        finalizedQueries.push(
+          db.query(queryStatement, [asset.id, asset.symbol, asset.name])
+        );
       }
 
-      return finalizedQueries
+      return finalizedQueries;
     } catch (err) {
       // log err
-      console.log(`${queryStatement}: `, err)
+      console.log(`${queryStatement}: `, err);
     }
   },
-}
+};
--- models/exchange-asset-pair-reference.js
+++ models/exchange-asset-pair-reference.js
@@ -1,9 +1,8 @@
-const db = require('../db')
+const db = require('../db');
 
 module.exports = {
-  createMany: async data => {
-    try {
-      const queryStatement = `INSERT INTO exchange_asset_pair_references (exchange_id, asset_symbol, quote_symbol, asset_id, quote_id, is_stale)
+  createMany: async (data) => {
+    const queryStatement = `INSERT INTO exchange_asset_pair_references (exchange_id, asset_symbol, quote_symbol, asset_id, quote_id, is_stale)
         SELECT
           (SELECT id FROM exchanges WHERE api_id = $1),
           $2,
@@ -21,26 +20,27 @@ module.exports = {
           quote_symbol = $3,
           asset_id = (SELECT id FROM assets WHERE symbol = $4),
           quote_id = (SELECT id FROM assets WHERE symbol = $5),
-          is_stale = $6;`
-      const finalizedQueries = []
+          is_stale = $6;`;
+    try {
+      const finalizedQueries = [];
 
-      for(let pairRef of data) {
-        finalizedQueries.push(db.query(queryStatement,
-          [
+      for (let pairRef of data) {
+        finalizedQueries.push(
+          db.query(queryStatement, [
             pairRef.exchangeId,
             pairRef.assetSymbol,
             pairRef.quoteSymbol,
             pairRef.assetSymbol,
             pairRef.quoteSymbol,
-            pairRef.isStale
-          ]
-        ))
+            pairRef.isStale,
+          ])
+        );
       }
 
-      return finalizedQueries
+      return finalizedQueries;
     } catch (err) {
       // log err
-      console.log('createMany exchanges error: ', err)
+      console.log(`${queryStatement}: `, err);
     }
   },
-}
+};
